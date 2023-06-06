116fe0d02b006099b15b3fd9fd4782e525677a0b
SamirOuarrak <samir.ouarrak@gmail.com>
2023-06-06 21:24:28 +0100

    AutoPush

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for letter in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(letter)), end="")
+    if (chr(letter) != 'q' and chr(letter) != 'e'):
+	    print("{}".format(chr(letter)), end="")
