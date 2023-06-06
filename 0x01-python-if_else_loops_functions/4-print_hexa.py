b53006f01a11041fc1c89c147a922743eb6c9481
SamirOuarrak <samir.ouarrak@gmail.com>
2023-06-06 21:31:13 +0100

    AutoPush

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 for num in range(0, 99):
-    if (chr(num) != 'q' and chr(num) != 'e'):
-        print("{} = {}".format(num, hex(num)), end="\n")
+    print("{} = {}".format(num, hex(num)), end="\n")
