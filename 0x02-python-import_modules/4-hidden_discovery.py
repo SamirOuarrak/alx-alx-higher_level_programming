0b78e31a9eff9268e447b36717ae683a990bfdf1
SamirOuarrak <samir.ouarrak@gmail.com>
2023-06-08 20:53:32 +0100

    AutoPush

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    name = dir(hidden_4)
+    for i in range(len(name)):
+        if name[i][0] != "_" and name[i][1] != "_":
+            print(name[i])
+
+
+if __name__ == "__main__":
+    main()
