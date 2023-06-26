d4e83617f98b05aa3d7e751ebb22feb6800429a9
SamirOuarrak <samir.ouarrak@gmail.com>
2023-06-26 18:01:54 +0100

    AutoPush

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            dividend = my_list_1[i]
+            divisor = my_list_2[i]
+            if not isinstance(dividend, (int, float)) or not isinstance(divisor, (int, float)):
+                raise TypeError
+            division = dividend / divisor
+        except ZeroDivisionError:
+            print("division by 0")
+            division = 0
+        except TypeError:
+            print("wrong type")
+            division = 0
+        except IndexError:
+            print("out of range")
+            division = 0
+        finally:
+            result.append(division)
+    return result
